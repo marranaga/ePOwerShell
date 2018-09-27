be4666f61dab99d7057cb753e8e0330cddffe32e
Michael Arranaga <Michael.Arranaga@unt.edu>
2018-09-27 14:06:14 -0500

    Added if statement to see if we should actually deploy to PSGallery

--- .scripts/deploy.ps1
+++ .scripts/deploy.ps1
@@ -6,12 +6,14 @@ $PSScriptRootParent = Split-Path $PSScriptRoot -Parent
 Write-Host "[Deploy] PSScriptRootParent: ${PSScriptRootParent}" -Foregroundcolor 'Blue' -BackgroundColor 'Magenta'
 Write-Host "[Deploy] APPVEYOR_PROJECT_NAME: ${env:APPVEYOR_PROJECT_NAME}" -Foregroundcolor 'Blue' -BackgroundColor 'Magenta'
 
-Deploy Module {
-    By PSGalleryModule ePOwerShell {
-        FromSource "${PSScriptRootParent}\dev\BuildOutput\ePOwerShell"
-        To PSGallery
-        WithOptions @{
-            ApiKey = $env:PSGalleryApiKey
+if ($env:APPVEYOR_REPO_TAG) {
+    Deploy Module {
+        By PSGalleryModule ePOwerShell {
+            FromSource "${PSScriptRootParent}\dev\BuildOutput\ePOwerShell"
+            To PSGallery
+            WithOptions @{
+                ApiKey = $env:PSGalleryApiKey
+            }
         }
     }
 }
